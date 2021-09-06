2800eadf9ea6c69f9ecf9adbbd76b6b265b8df55
Artur Dias Ribeiro <Arturdias1010@hotmail.com>
2021-09-06 15:42:59 -0300

    Projeto criado

---
+++ src/main/java/com/useall/useall/UseallApplication.java
@@ -0,0 +1,13 @@
+package com.useall.useall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UseallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UseallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/useall/useall/UseallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.useall.useall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UseallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
